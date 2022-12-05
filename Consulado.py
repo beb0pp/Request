e0f455fbf69850a702a2b75ef8b145b70892f206
beb0pp <luss.fel@gmail.com>
2022-12-04 22:18:49 -0300

    2 Atualizaçao do codigo Request

--- Consulado.py
+++ Consulado.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-
 date_now = datetime.now()
 diaanterior = date_now - timedelta(1)
 diaanterior = diaanterior.strftime('%d/%m/%Y')
@@ -21,8 +20,8 @@ chrome_options = Options()
 # chrome_options.add_argument('--no-sandbox')
 # chrome_options.add_argument('--disable-dev-shm-usage')
 
-chrome_options.add_experimental_option("prefs", {"download.default_directory": r"Q:\Risco\Novo Risco\pythonrisco\Codigos\data\ValidacaoINOA"})
-driver = webdriver.Chrome("Q://Risco//Novo Risco//pythonrisco//Codigos//ChromeDriver//chromedriver.exe", chrome_options=chrome_options)
+# chrome_options.add_experimental_option("prefs", {"download.default_directory": r"Q:\Risco\Novo Risco\pythonrisco\Codigos\data\ValidacaoINOA"})
+driver = webdriver.Chrome(r"C:\\Users\\luisf\\OneDrive\\Área de Trabalho\\Estudos Python\\ChromeDriver\\chromedriver.exe", chrome_options=chrome_options)
 driver.implicitly_wait(5)
 driver.maximize_window()
 
@@ -36,19 +35,24 @@ driver.find_element(By.XPATH, '//*[@id="sign_in_form"]/div[3]/label/div').click(
 driver.find_element(By.XPATH, '//*[@id="sign_in_form"]/p[1]/input').click()
 time.sleep(3)
 
-driver.find_element(By.XPATH, '//*[@id="main"]/div[2]/div[3]/div[1]/div/div[1]/div[1]/div[2]/ul/li/a').click()
+driver.find_element(By.XPATH, '/html/body/div[4]/main/div[2]/div[3]/div[1]/div/div[2]/div[1]/div[2]/ul/li/a').click()
 
 
-driver.find_element(By.XPATH, '//*[@id="tvktw8-accordion-label"]').click()
+driver.find_element(By.XPATH, '//*[@id="3quk1q-accordion-label"]').click()
 time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="tvktw8-accordion"]/div/div[2]/p[2]/a').click()
+driver.find_element(By.XPATH, '/html/body/div[4]/main/div[2]/div[2]/div/section/ul/li[4]/div/div/div[2]/p[2]/a').click()
 time.sleep(2)
 
 agendamento = driver.find_element(By.XPATH, '//*[@id="appointments_consulate_appointment_date"]').click()
 agendamento = driver.find_element(By.XPATH, '//*[@id="appointments_consulate_appointment_date"]').get_attribute('value')
 
-agendamento = datetime.strptime(agendamento, '%Y-%m-%d')
-dataselecionada = datetime.strptime(input('Insira uma data:  (formato: yyyy/mm/dd)'), '%Y-%m-%d')
+dataMaxima = datetime.strptime(input('Insira uma data:  (formato: yyyy/mm/dd)'), '%Y-%m-%d')
+
+meses = {'January' : '01', 'February' : '02', 'March' : '03', 'April' : '04', 'May' : '05', 'June' : '06', 'July' : '07', 'August' : '08', 'September' : '09', 'October' : '10', 'November' : '11', 'December' : '12'}
+
+dezembro = driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div[1]/div/div/span[1]').text
+janeiro = driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div[2]/div/div/span[1]').text
+
 
 while True:
     try:
@@ -68,7 +72,8 @@ while True:
         except:
             driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div[2]/div/a').click()
             
-    if dataselecionada > agendamento:
+    if agendamento <= dataMaxima:
         print('show')
     else:
         print('Nok')
+    break
